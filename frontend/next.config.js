eaf76463114e830027a7428cdd74ebfc5ffed87d
muriloleal13 <msleal@inf.ufsm.br>
2025-08-31 09:32:20 -0300

    chore: deploy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
-  },
   output: 'export',
 
   // Configurações para melhorar hidratação
