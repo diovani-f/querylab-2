fe5ffab26436b055366e900939c8ef4aec2b6383
muriloleal13 <msleal@inf.ufsm.br>
2025-08-31 09:34:12 -0300

    fix: path alias webpack

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
   output: 'export',
 
   // Configurações para melhorar hidratação
