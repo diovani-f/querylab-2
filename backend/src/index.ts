3bb7d4b411cf47ca28b2eb86012f97b7604e6506
Diovani <diovani@example.com>
2026-03-06 18:51:07 -0300

    Fix: add frontend url to backend cors allowed Origins list

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -41,7 +41,8 @@ const PORT = process.env.PORT || 5000
 
 const allowedOrigins = [
   process.env.FRONTEND_URL || 'http://localhost:3000',
-  'http://localhost:3000' // front local
+  'http://localhost:3000', // front local
+  'https://querylab-2.onrender.com' // front em producao
 ];
 
 // Middlewares
